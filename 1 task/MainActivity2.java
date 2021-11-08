1ff9864c4d9ac9dab72e6d4b18b3314755899d0a
HanyBin <51113403+HanyBin@users.noreply.github.com>
2021-11-08 11:13:46 +0300

    Add files via upload

--- 1 task/MainActivity2.java
+++ 1 task/MainActivity2.java
@@ -6,21 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         findViewById(R.id.btnActTwo).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                        Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
                         startActivity(intent);
                     }
                 }
         );
     }
 }
-
--- 1 task/MainActivity3.java
+++ 1 task/MainActivity3.java
@@ -2,25 +2,24 @@ package com.example.md17;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
         findViewById(R.id.btnActTwo).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                        Intent intent = new Intent(MainActivity3.this, MainActivity4.class);
                         startActivity(intent);
                     }
                 }
         );
     }
 }
-
